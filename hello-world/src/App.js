b1005e7f12c6bc45f9389d3d75735fc68ea0046b
Pradeep712 <pradeep7mail@gmail.com>
2020-08-02 14:21:37 +0530

    Step 4 Statefull ClassComponent

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Greet}  from './component/Greet';
+import Welcome from './component/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
