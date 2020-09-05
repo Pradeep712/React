ab226979e5fba067818ec50165bd467852627bdb
Pradeep712 <pradeep7mail@gmail.com>
2020-09-05 16:25:59 +0530

    Step 30 ReactHttp_1

--- hello-world/src/component/PostList.js
+++ hello-world/src/component/PostList.js
@@ -6,23 +6,32 @@ class PostList extends Component {
         super(props)
     
         this.state = {
-            post : []
+            posts : []
         }
     }
     componentDidMount(){
         axios.get("https://jsonplaceholder.typicode.com/posts")
         .then(response =>{
             console.log(response)
+            this.setState({posts: response.data})
         })
         .catch(error=>{
             console.log(error)
+            this.setState({errorMsg: "Error in Retreiving Data"})
         })
     }
 
     render() {
+        const {posts , errorMsg} =this.state
         return (
             <div>
                 List of posts
+                {
+                    posts.length ? posts.map(post => <div key={post.id}>{post.id}. {post.title}</div>):null
+                }
+                {
+                    errorMsg ? <div>{errorMsg}</div>:null
+                }
             </div>
         )
     }
