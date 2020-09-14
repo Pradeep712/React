32d860ec25c1bb57f59d471b455381e7ff1ac415
Pradeep712 <pradeep7mail@gmail.com>
2020-09-14 21:11:44 +0530

    Step 33 ReactHook with Object using setter function

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import HookCounterTwo from './component/HookCounterTwo';
+import HookCounterThree from './component/HookCounterThree';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <HookCounterTwo />
+        <HookCounterThree />
       </div>
     )
   }
